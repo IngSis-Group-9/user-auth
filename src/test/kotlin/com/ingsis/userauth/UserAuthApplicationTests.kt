75372abd21ee025a98a1a23f8f88e0497902e184
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2024-05-14 11:47:17 -0300

    Spring Boot project with docker-compose and DB

---
+++ src/test/kotlin/com/ingsis/userauth/UserAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ingsis.userauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserAuthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
